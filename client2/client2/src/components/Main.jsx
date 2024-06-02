abd5aab31e05af9d3a57684c4a8edd913c86d6a5
kelechukwufavour <kelechukwufavour.faye@gmail.com>
2024-06-02 18:17:01 +0100

    playlist and search placeholders added

--- client2/client2/src/components/Main.jsx
+++ client2/client2/src/components/Main.jsx
@@ -1,20 +1,41 @@
 import React from 'react'
 import Player from './Player'
-
-// import '../styles/styles.css';
+// import './styles.css';
 
 const Main = () => {
   return (
-    <section >
-    
+    <section>
+      <Player />
 
-    <Player/>
+      <div className='database'>
+        <h1> PlayList</h1>
 
-    <div> 
-    <h1> PlayList</h1>
-    <div> 
-    <img src:  /></div>
-    </div>
+        <div className="search-input-wrapper">
+          <input type="text" placeholder="Search.." name="query" />
+          <img src='./src/assets/search.svg' className="search-icon" alt="search icon"/>
+        </div>
+
+        <div className='database-contents'>
+          <img src='./src/assets/zoom.svg' alt="zoom"/>
+          <p>Big Buck Bunny</p>
+        </div>
+        <div className='database-contents'>
+          <img src='./src/assets/zoom.svg' alt="zoom"/>
+          <p>Big Buck Bunny</p>
+        </div>
+        <div className='database-contents'>
+          <img src='./src/assets/zoom.svg' alt="zoom"/>
+          <p>Big Buck Bunny</p>
+        </div>
+        <div className='database-contents'>
+          <img src='./src/assets/zoom.svg' alt="zoom"/>
+          <p>Big Buck Bunny</p>
+        </div>
+        <div className='database-contents'>
+          <img src='./src/assets/zoom.svg' alt="zoom"/>
+          <p>Big Buck Bunny</p>
+        </div>
+      </div>
     </section>
   )
 }
